40871e88207f4a820594f24f8bafebd656f92dd3
unknown <ninguemd75@gmail.com>
2024-08-25 03:57:37 -0300

    tela perfil/produto responsiva

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     @include('includes.head')
     <style>
@@ -46,6 +47,7 @@
         }
     </style>
 </head>
+
 <body>
     @include('includes.header')
     @include('includes.nav')
@@ -54,9 +56,19 @@
         <div class="container-perfil">
             <div class="frame-perfil">
                 <div class="img-topo-perfil">
-                    <div class="foto-perfil">
-                        <img src="{{ url($imagemCliente) }}" alt="Foto do Perfil" class="img-fluid rounded-circle" style="width: 100%; height: 100%; object-fit: cover;">
+                    <div class="frame-foto-perfil">
+                        <div class="foto-perfil">
+                            <img src="{{ url($imagemCliente) }}" alt="Foto do Perfil" class="img-fluid rounded-circle"
+                                style="width: 100%; height: 100%; object-fit: cover;">
+                        </div>
                     </div>
+                </div>
+                <div class="nome-usuario">
+                    <p style="margin:0">Matheus Santos Silva Da Cruz Machado Nascimento Gomes Pereira Leite</p>
+                </div>
+                <div class="opcoes-usuario">
+                    <p style="margin:0; padding: 1vh">Meus favoritos &hearts;</p>
+                    <p style="margin:0; padding: 1vh">Carrinho</p>
                 </div>
             </div>
             <div class="align-info">
@@ -66,55 +78,64 @@
                 </div>
 
                 <div class="info-perfil">
-                    <form action="{{ url('/perfil/update') }}" method="POST" enctype="multipart/form-data">
+                    <form action="{{ url('/perfil/update') }}" method="POST" enctype="multipart/form-data"
+                        class="form-info">
                         @csrf
-                        <div class="titulo-info">
-                            <div class="titulo-info-font">
-                                <h5 class="info-texto">Dados Pessoais</h5>
-                            </div>
-                            <div class="container-info">
-                                <ul class="lista-info">
-                                    <li class="lista-item">Nome</li>
-                                    <li class="lista-item">E-mail</li>
-                                    <li class="lista-item">Telefone</li>
-                                </ul>
-                                <ul class="lista-info">
-                                    <li class="lista-item"><input type="text" name="nomeCliente" value="{{ $nome }}"></li>
-                                    <li class="lista-item"><input type="email" name="emailCliente" value="{{ $email }}"></li>
-                                    <li class="lista-item"><input type="text" name="numeroCliente" value="{{ $numero }}"></li>
-                                </ul>
-                            </div>
 
-                            <!-- Campo para upload da imagem -->
-                            <div class="form-group">
-                                <label for="imagemCliente">Atualizar imagem de perfil:</label>
-                                <input type="file" name="imagemCliente" class="form-control" id="imagemCliente" style="width: 200px;">
-                            </div>
-
-                            <div class="titulo-info-font">
-                                <h5 class="info-texto">Endereços</h5>
-                            </div>
-                            <div class="container-info">
-                                <ul class="lista-info">
-                                    <li class="lista-item">Endereço</li>
-                                    <li class="lista-item">CEP</li>
-                                    <li class="lista-item">Cidade</li>
-                                    <li class="lista-item">Estado</li>
-                                </ul>
-                                <ul class="lista-info">
-                                    <li class="lista-item"><input type="text" name="logradouroCliente" value="{{ $logradouro }}"></li>
-                                    <li class="lista-item"><input type="text" name="cepCliente" value="{{ $cep }}"></li>
-                                    <li class="lista-item"><input type="text" name="cidadeCliente" value="{{ $cidade }}"></li>
-                                    <li class="lista-item"><input type="text" name="estadoCliente" value="{{ $estado }}"></li>
-                                </ul>
-                            </div>
+                        <div class="titulo-info-font">
+                            <h5 class="info-texto">Dados Pessoais</h5>
                         </div>
-                        <button type="submit" class="btn btn-primary" style="background-color: #5e3e24; margin-top: 70%;">Salvar alterações</button>
+                        <div class="container-info">
+                            <ul class="lista-info">
+                                <li class="lista-item">Nome</li>
+                                <li class="lista-item">E-mail</li>
+                                <li class="lista-item">Telefone</li>
+                            </ul>
+                            <ul class="lista-info">
+                                <li class="lista-item"><input type="text" name="nomeCliente" value="{{ $nome }}"></li>
+                                <li class="lista-item"><input type="email" name="emailCliente" value="{{ $email }}">
+                                </li>
+                                <li class="lista-item"><input type="text" name="numeroCliente" value="{{ $numero }}">
+                                </li>
+                            </ul>
+                        </div>
+
+                        <!-- Campo para upload da imagem -->
+                        <div class="form-group">
+                            <label for="imagemCliente">Atualizar imagem de perfil:</label>
+                            <input type="file" name="imagemCliente" class="form-control" id="imagemCliente">
+                        </div>
+
+                        <div class="titulo-info-font">
+                            <h5 class="info-texto">Endereços</h5>
+                        </div>
+                        <div class="container-info">
+                            <ul class="lista-info">
+                                <li class="lista-item">Endereço</li>
+                                <li class="lista-item">CEP</li>
+                                <li class="lista-item">Cidade</li>
+                                <li class="lista-item">Estado</li>
+                            </ul>
+                            <ul class="lista-info">
+                                <li class="lista-item"><input type="text" name="logradouroCliente"
+                                        value="{{ $logradouro }}"></li>
+                                <li class="lista-item"><input type="text" name="cepCliente" value="{{ $cep }}"></li>
+                                <li class="lista-item"><input type="text" name="cidadeCliente" value="{{ $cidade }}">
+                                </li>
+                                <li class="lista-item"><input type="text" name="estadoCliente" value="{{ $estado }}">
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="container-submit">
+                            <button type="submit" class="btn btn-primary" style="background-color: #5e3e24;">Salvar
+                                alterações</button>
+                            <p class="excluir-txt">Excluir minha conta</p>
+                            <p class="aviso-excluir">Ao excluir a conta, torna-se impossível recuperá-la.</p>
+                        </div>
                     </form>
-                    <div class="container">
-                        <p class="excluir-txt">Excluir minha conta</p>
-                        <p class="aviso-excluir">Ao excluir a conta, torna-se impossível recuperá-la.</p>
-                    </div>
+
+
+
                 </div>
             </div>
         </div>
@@ -125,8 +146,15 @@
     <!-- Import do javascript -->
     <script src="{{ url('js/script.js') }}"></script>
     <!-- Imports do bootstrap do body -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     @include('includes.head')
 </head>
+
 <body>
     @include('includes.header')
     @include('includes.nav')
@@ -13,9 +15,9 @@
             <div class="produto-line"></div>
             <div class="filtro-produtos">
                 <p class="filtro-display">Filtrado por:</p>
-                <div class="filtro-itens">Roupas &#10005</div>
-                <div class="filtro-itens">Casacos &#10005</div>
-                <div class="filtro-itens">GG &#10005</div>
+                <div class="filtro-itens">1</div>
+                <div class="filtro-itens">2</div>
+                <div class="filtro-itens">3</div>
             </div>
         </div>
         <div class="produtos-body">
@@ -28,9 +30,9 @@
                             De
                             <input type="preco" class="preco-filtro">
                         </label>
-                        <label for="text" class="label-condicao">
+                        <label for="text" class="label-preco">
                             Até
-                            <input type="preco" class="label-preco">
+                            <input type="preco" class="preco-filtro">
                         </label>
                     </div>
 
@@ -105,7 +107,8 @@
                     @foreach($produtos as $produto)
                         <div class="card-produto">
                             <div class="image-display">
-                                <img class="img-card-produto" src="{{ asset($produto->imagemProduto) }}" alt="{{ $produto->nomeProduto }}">
+                                <img class="img-card-produto" src="{{ asset($produto->imagemProduto) }}"
+                                    alt="{{ $produto->nomeProduto }}">
                             </div>
                             <div class="txt-info-vendedor">
                                 <p></p>
@@ -117,16 +120,19 @@
                             <p class="valor-produto">{{ $produto->valorProduto }}</p>
                             <div class="txt-info-produto">
                                 <div class="row-info">
-                                    <p>{{ $produto->cor->nomeCor ?? 'Cor Desconhecida' }}</p>
-                                    <p>{{ $produto->condicao->nomeCondicao ?? 'Condição Desconhecida' }}</p>
+                                    <p class="info-produto">{{ $produto->cor->nomeCor ?? 'Cor Desconhecida' }}</p>
+                                    <p class="info-produto">
+                                        {{ $produto->condicao->nomeCondicao ?? 'Condição Desconhecida' }}</p>
                                 </div>
                                 <div class="row-info">
-                                    <p>{{ $produto->categoria->nomeCategoriaProduto ?? 'Categoria Desconhecida' }}</p>
-                                    <p>{{ $produto->tamanho->nomeTamanho ?? 'Tamanho Desconhecido' }}</p>
+                                    <p class="info-produto">
+                                        {{ $produto->categoria->nomeCategoriaProduto ?? 'Categoria Desconhecida' }}</p>
+                                    <p class="info-produto">{{ $produto->tamanho->nomeTamanho ?? 'Tamanho Desconhecido' }}
+                                    </p>
                                 </div>
                                 <div class="row-info">
-                                   
-                                    <p>{{ $produto->regiao->nomeRegiao ?? 'Região Desconhecida' }}</p>
+
+                                    <p class="info-produto">{{ $produto->regiao->nomeRegiao ?? 'Região Desconhecida' }}</p>
                                 </div>
                             </div>
                         </div>
@@ -137,20 +143,20 @@
 
         <!-- Script para animação do accordion do filtro -->
         <script>
-        var acc = document.getElementsByClassName("accordion");
-        var i;
+            var acc = document.getElementsByClassName("accordion");
+            var i;
 
-        for (i = 0; i < acc.length; i++) {
-            acc[i].addEventListener("click", function() {
-                this.classList.toggle("active");
-                var panel = this.nextElementSibling;
-                if (panel.style.maxHeight) {
-                    panel.style.maxHeight = null;
-                } else {
-                    panel.style.maxHeight = panel.scrollHeight + "px";
-                }
-            });
-        }
+            for (i = 0; i < acc.length; i++) {
+                acc[i].addEventListener("click", function () {
+                    this.classList.toggle("active");
+                    var panel = this.nextElementSibling;
+                    if (panel.style.maxHeight) {
+                        panel.style.maxHeight = null;
+                    } else {
+                        panel.style.maxHeight = panel.scrollHeight + "px";
+                    }
+                });
+            }
         </script>
     </main>
     <!-- Fim do corpo do site -->
@@ -160,8 +166,15 @@
     <!-- Import do JavaScript -->
     <script src="{{ asset('js/script.js') }}"></script>
     <!-- Imports do Bootstrap -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>

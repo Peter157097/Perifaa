63c5cdf10798eb608bf4a1a9c112b4dfa65128ef
Eduardo <eeduardosilva010506@gmail.com>
2024-08-28 22:20:13 -0300

    Eduardo: logica carrinho

---
+++ app/Models/Carrinho.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Carrinho extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tbcarrinho';
+
+    protected $primaryKey = 'idCarrinho';
+
+    // Desativa o gerenciamento automático de timestamps
+    public $timestamps = false;
+
+    // Define os atributos que podem ser preenchidos em massa
+    protected $fillable = ['idCliente', 'idProduto'];
+    
+    // Define o relacionamento com o modelo Post
+    public function product()
+    {
+        return $this->belongsTo(Produto::class, 'idProduto');
+    }
+
+    // Define o relacionamento com o modelo Cliente
+    public function client()
+    {
+        return $this->belongsTo(Cliente::class, 'idCliente');
+    }
+}

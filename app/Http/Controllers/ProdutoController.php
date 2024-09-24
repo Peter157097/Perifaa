e72f873ab5e558215e543ac1f960eb106cd5dab9
Peter <peterdeoliveira99@gmail.com>
2024-09-24 17:32:31 -0300

    Filtros completos

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -16,51 +16,44 @@ class ProdutoController extends Controller
 {
     public function index(Request $request)
     {
-        // Inicia a query de produtos
         $query = Produto::query();
     
-        /* Aplica filtro de preço mínimo, se fornecido
-        if ($request->filled('preco_de')) {
-            $query->where('valorProduto', '>=', $request->input('preco_de'));
-        }*/
-        // Aplica filtro de preço máximo, se fornecido
+        // Filtros
         if ($request->filled('preco_ate')) {
             $precoAte = (float) $request->input('preco_ate');
-    
             $query->where('valorProduto', '<=', $precoAte);
         }
 
-
         if ($request->filled('tamanho')) {
-            $tamanhoId = $request->input('tamanho');
-            $query->where('idTamanho', $tamanhoId);
+            $tamanhoIds = $request->input('tamanho');
+            $query->whereIn('idTamanho', $tamanhoIds);
         }
-    
 
         if ($request->filled('condicoes')) {
             $condicoes = $request->input('condicoes');
             $query->whereIn('idCondicao', $condicoes);
         }
-    
 
         if ($request->filled('cores')) {
             $cores = $request->input('cores');
             $query->whereIn('idCor', $cores);
         }
-    
+
         if ($request->filled('regioes')) {
             $regioes = $request->input('regioes');
             $query->whereIn('idRegiao', $regioes);
         }
 
+        // Pegar dados
         $tamanhos = Tamanho::all();
         $condicoes = Condicao::all();
-        $cores = Cor::all(); 
+        $cores = Cor::all();
         $regioes = Regiao::all();
-        $produtos = $query->paginate(8); 
+        $produtos = $query->paginate(8);
+        $filtros = $request->all();
     
 
-        return view('produtos', compact('produtos', 'tamanhos','condicoes', 'cores','regioes'));
+        return view('produtos', compact('produtos', 'tamanhos','condicoes', 'cores','regioes','filtros'));
     }
 
 

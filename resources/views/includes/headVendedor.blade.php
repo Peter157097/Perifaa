58bb3735aa0860ab78290aa9e1704e26ecf1cd5e
Rafinha <131063922+RafaBertacelli@users.noreply.github.com>
2024-09-30 23:14:50 -0300

    Update headVendedor.blade.php

--- resources/views/includes/headVendedor.blade.php
+++ resources/views/includes/headVendedor.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/x-icon">
     <title>Perifa</title>
     <link href="https://fonts.googleapis.com/css2?family=Bubblegum+Sans&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">

e7071029d234fe650a97a2c79b29ad2390406115
unknown <ninguemd75@gmail.com>
2024-10-20 21:39:38 -0300

    tela de usuario sem login /mensagens e /pedidos - tela caso n tenha conversa selecionada no /mensagens

--- resources/views/mensagens.blade.php
+++ resources/views/mensagens.blade.php
@@ -12,21 +12,19 @@
     <main>
         <div class="mensagensContainer">
             @if(session('is_Cliente'))
+                <div class="topSubNav">
+                    <ul class="subNavItens">
+                        <li><a href="/pedidos">Pedidos</a></li>
+                        <li><a href="/mensagens">Mensagens</a></li>
+                    </ul>
+                </div>
                 <div class="containerMensagens">
-                    <div class="topSubNav">
-                        <ul class="subNavItens">
-                            <li><a href="/pedidos">Pedidos</a></li>
-                            <li><a href="/mensagens">Mensagens</a></li>
-                        </ul>
+                    <div class="msgTitlePanel">
+                        Mensagens
                     </div>
                     <div class="panelMensagens">
                         <div class="sidePanelMensagens">
-
-                            <div class="msgTitlePanel">
-                                Mensagens
-                            </div>
                             <div class="scroll-sidePanelMsg">
-                               
                                 <div class="cardMensagem">
                                     <div class="pfpMsgContainer">
                                         <div class="pfpMsg">
@@ -212,6 +210,21 @@
                             </div>
                         </div>
                         <div class="contentMsg">
+
+                            <!--  
+
+                                                    ----Caso nao tenha mensagem selecionada (Parte do cliente)----
+
+                                                        <div class="noContent">
+                                                            <div class="HoldNoContentImg">
+                                                                <img src="{{url('images/msgIconCli.png')}}" class="noContentImg">
+                                                            </div>
+                                                            <div class="noContentMsg">
+                                                                <h4>Suas mensagens</h4>
+                                                                <p>Aqui é onde você se conecta com outros usuários.</p>
+                                                            </div>
+                                                        </div>
+                                                    -->
                             <div class="selectedMsg">
                                 <div class="pfpSelectedMsg">
                                     <div class="pfpMsg">
@@ -240,6 +253,7 @@
                                     </div>
                                 </div>
                             </div>
+
                         </div>
                     </div>
                 </div>
@@ -251,11 +265,11 @@
                     </ul>
                 </div>
                 <div class="containerMensagens">
+                    <div class="msgTitlePanel">
+                        Mensagens
+                    </div>
                     <div class="panelMensagens">
                         <div class="sidePanelMensagens">
-                            <div class="msgTitlePanel">
-                                Mensagens
-                            </div>
                             <div class="scroll-sidePanelMsg">
                                 <div class="cardMensagem">
                                     <div class="pfpMsgContainer">
@@ -392,10 +406,24 @@
                                         </div>
                                     </div>
                                 </div>
-                                <div class="shadowScroll"></div>
+
                             </div>
                         </div>
                         <div class="contentMsg">
+                            <!--  
+
+                                            ----Caso nao tenha mensagem selecionada (parte do vendedor)----
+
+                                                <div class="noContent">
+                                                    <div class="HoldNoContentImg">
+                                                        <img src="{{url('images/msgIconVend.png')}}" class="noContentImg">
+                                                    </div>
+                                                    <div class="noContentMsg">
+                                                        <h4>Suas mensagens</h4>
+                                                        <p>Aqui é onde você se conecta com outros usuários.</p>
+                                                    </div>
+                                                </div>
+                                            -->
                             <div class="selectedMsg">
                                 <div class="pfpSelectedMsg">
                                     <div class="pfpMsg">
@@ -581,6 +609,20 @@
                         </div>
                         <div class="contentMsg">
                             <div class="selectedMsg">
+                                <!--  
+
+                                        ----Caso nao tenha mensagem selecionada (parte do Admin)----
+
+                                            <div class="noContent">
+                                                <div class="HoldNoContentImg">
+                                                    <img src="{{url('images/msgIconAdmin.png')}}" class="noContentImg">
+                                                </div>
+                                                <div class="noContentMsg">
+                                                    <h4>Suas mensagens</h4>
+                                                    <p>Aqui é onde você se conecta com outros usuários.</p>
+                                                </div>
+                                            </div>
+                                        -->
                                 <div class="pfpSelectedMsg">
                                     <div class="pfpMsg">
                                         <img src="{{url('images/10.png')}}" class="pfpConsulta">
@@ -619,181 +661,16 @@
                     </ul>
                 </div>
                 <div class="containerMensagens">
-                    <div class="panelMensagens">
-                        <div class="sidePanelMensagens">
-                            <div class="msgTitlePanel">
-                                Mensagens
-                            </div>
-                            <div class="scroll-sidePanelMsg">
-                                <div class="cardMensagem">
-                                    <div class="pfpMsgContainer">
-                                        <div class="pfpMsg">
-                                            <img src="{{url('images/10.png')}}" class="pfpConsulta">
-                                        </div>
-                                    </div>
-                                    <div class="containerMsg">
-                                        <div class="titleMsg">
-                                            Julia S.
-                                        </div>
-                                        <div class="bodyMsg">
-                                            Oiii, bom dia, queria saber se ainde tem essa peça, gostei mt dela
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="cardMensagem">
-                                    <div class="pfpMsgContainer">
-                                        <div class="pfpMsg">
-                                            <img src="{{url('images/10.png')}}" class="pfpConsulta">
-                                        </div>
-                                    </div>
-                                    <div class="containerMsg">
-                                        <div class="titleMsg">
-                                            Julia S.
-                                        </div>
-                                        <div class="bodyMsg">
-                                            Oiii, bom dia, queria saber se ainde tem essa peça, gostei mt dela
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="cardMensagem">
-                                    <div class="pfpMsgContainer">
-                                        <div class="pfpMsg">
-                                            <img src="{{url('images/10.png')}}" class="pfpConsulta">
-                                        </div>
-                                    </div>
-                                    <div class="containerMsg">
-                                        <div class="titleMsg">
-                                            Julia S.
-                                        </div>
-                                        <div class="bodyMsg">
-                                            Oiii, bom dia, queria saber se ainde tem essa peça, gostei mt dela
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="cardMensagem">
-                                    <div class="pfpMsgContainer">
-                                        <div class="pfpMsg">
-                                            <img src="{{url('images/10.png')}}" class="pfpConsulta">
-                                        </div>
-                                    </div>
-                                    <div class="containerMsg">
-                                        <div class="titleMsg">
-                                            Julia S.
-                                        </div>
-                                        <div class="bodyMsg">
-                                            Oiii, bom dia, queria saber se ainde tem essa peça, gostei mt dela
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="cardMensagem">
-                                    <div class="pfpMsgContainer">
-                                        <div class="pfpMsg">
-                                            <img src="{{url('images/10.png')}}" class="pfpConsulta">
-                                        </div>
-                                    </div>
-                                    <div class="containerMsg">
-                                        <div class="titleMsg">
-                                            Julia S.
-                                        </div>
-                                        <div class="bodyMsg">
-                                            Oiii, bom dia, queria saber se ainde tem essa peça, gostei mt dela
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="cardMensagem">
-                                    <div class="pfpMsgContainer">
-                                        <div class="pfpMsg">
-                                            <img src="{{url('images/10.png')}}" class="pfpConsulta">
-                                        </div>
-                                    </div>
-                                    <div class="containerMsg">
-                                        <div class="titleMsg">
-                                            Julia S.
-                                        </div>
-                                        <div class="bodyMsg">
-                                            Oiii, bom dia, queria saber se ainde tem essa peça, gostei mt dela
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="cardMensagem">
-                                    <div class="pfpMsgContainer">
-                                        <div class="pfpMsg">
-                                            <img src="{{url('images/10.png')}}" class="pfpConsulta">
-                                        </div>
-                                    </div>
-                                    <div class="containerMsg">
-                                        <div class="titleMsg">
-                                            Julia S.
-                                        </div>
-                                        <div class="bodyMsg">
-                                            Oiii, bom dia, queria saber se ainde tem essa peça, gostei mt dela
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="cardMensagem">
-                                    <div class="pfpMsgContainer">
-                                        <div class="pfpMsg">
-                                            <img src="{{url('images/10.png')}}" class="pfpConsulta">
-                                        </div>
-                                    </div>
-                                    <div class="containerMsg">
-                                        <div class="titleMsg">
-                                            Julia S.
-                                        </div>
-                                        <div class="bodyMsg">
-                                            Oiii, bom dia, queria saber se ainde tem essa peça, gostei mt dela
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="cardMensagem">
-                                    <div class="pfpMsgContainer">
-                                        <div class="pfpMsg">
-                                            <img src="{{url('images/10.png')}}" class="pfpConsulta">
-                                        </div>
-                                    </div>
-                                    <div class="containerMsg">
-                                        <div class="titleMsg">
-                                            Julia S.
-                                        </div>
-                                        <div class="bodyMsg">
-                                            Oiii, bom dia, queria saber se ainde tem essa peça, gostei mt dela
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="shadowScroll"></div>
-                            </div>
-
+                    <div class="msgTitlePanel">
+                        Mensagens
+                    </div>
+                    <div class="containerNoRegister">
+                        <div class="noRegisterImg">
+                            <img src="{{url('images/msgSemRegistro.png')}}" class="noContentImg">
                         </div>
-
-                        <div class="contentMsg">
-                            <div class="selectedMsg">
-                                <div class="pfpSelectedMsg">
-                                    <div class="pfpMsg">
-                                        <img src="{{url('images/10.png')}}" class="pfpConsulta">
-                                    </div>
-                                </div>
-                                <div class="infoSelectecMsg">
-                                    <div class="titleSelectedImg">
-                                        Julia S.
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="msgDisplay">
-
-                            </div>
-                            <div class="msgWrite">
-                                <div class="contentWrite">
-                                    <div class="anexoMsg">
-                                        <img src="{{url('images/anexoMsg.png')}}" class="iconMsg" title="Anexar arquivo">
-                                    </div>
-                                    <div class="inputMsg">
-                                        <input type="text" placeholder="Mensagem..." class="inputCampo">
-                                    </div>
-                                    <div class="anexoMsg">
-                                        <img src="{{url('images/sendMsg.png')}}" class="iconMsg" title="Enviar">
-                                    </div>
-                                </div>
-                            </div>
+                        <div class="noRegisterMsg">
+                            <h4>Oops...</h4>
+                            <p>Faça <a href="">login</a> ou <a href="">cadastre-se</a> para ver suas mensagems.</p>
                         </div>
                     </div>
                 </div>
--- resources/views/pedidos.blade.php
+++ resources/views/pedidos.blade.php
@@ -6,6 +6,7 @@
     @include('includes.header')
 
     @include('includes.nav')
+
     <main>
         <div class="pedidosContainer">
             <div class="topSubNav">
@@ -14,75 +15,90 @@
                     <li><a href="/mensagens">Mensagens</a></li>
                 </ul>
             </div>
+
             <div class="containerMensagens">
-                <div class="panelPedidos">
-                    <div class="pedidosTitlePanel">
-                        Seus pedidos
-                    </div>
-                    <div class="itensPedidosContainer">
-                        <div class="itensWrap">
-                            <div class="cardPedido">
-                                <div class="imgProdutoPedido">
-                                    <img src="{{url('images/teste.jpg')}}">
-                                </div>
-                                <div class="infoProdutoPedido">
-                                    <div class="infoPedido">
-                                        <ul class="labelPedido">
-                                            <li>Vestindo a Quebrada</li>
-                                            <li>Nike Tech Fleece</li>
-                                            <li>R$49,90</li>
-                                        </ul>
-                                        <ul class="statusPedido">
-                                            <li class="sttsPedido">Status</li>
-                                            <li class="situacaoPedido">Enviado</li>
-                                        </ul>
+                @if(session('is_Cliente'))
+                    <div class="panelPedidos">
+                        <div class="pedidosTitlePanel">
+                            Seus pedidos
+                        </div>
+                        <div class="itensPedidosContainer">
+                            <div class="itensWrap">
+                                <div class="cardPedido">
+                                    <div class="imgProdutoPedido">
+                                        <img src="{{ url('images/teste.jpg') }}">
                                     </div>
-                                </div>
-                            </div>
-                            <div class="cardPedido">
-                                <div class="imgProdutoPedido">
-                                    <img src="{{url('images/teste.jpg')}}">
-                                </div>
-                                <div class="infoProdutoPedido">
-                                    <div class="infoPedido">
-                                        <ul class="labelPedido">
-                                            <li>Vestindo a Quebrada</li>
-                                            <li>bora bill</li>
-                                            <li>R$49,90</li>
-                                        </ul>
-                                        <ul class="statusPedido">
-                                            <li class="sttsPedido">Status</li>
-                                            <li class="situacaoPedido">Enviado</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="cardPedido">
-                                <div class="imgProdutoPedido">
-                                    <img src="{{url('images/teste.jpg')}}">
-                                </div>
-                                <div class="infoProdutoPedido">
-                                    <div class="infoPedido">
-                                        <ul class="labelPedido">
-                                            <li>Vestindo a Quebrada</li>
-                                            <li>bora bill</li>
-                                            <li>R$49,90</li>
-                                        </ul>
-                                        <ul class="statusPedido">
-                                            <li class="sttsPedido">Status</li>
-                                            <li class="situacaoPedido">Enviado</li>
-                                        </ul>
+                                    <div class="infoProdutoPedido">
+                                        <div class="infoPedido">
+                                            <ul class="labelPedido">
+                                                <li>Vestindo a Quebrada</li>
+                                                <li>Nike Tech Fleece</li>
+                                                <li>R$49,90</li>
+                                            </ul>
+                                            <ul class="statusPedido">
+                                                <li class="sttsPedido">Status</li>
+                                                <li class="situacaoPedido">Enviado</li>
+                                            </ul>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                @elseif(session('is_vendedor'))
+                    <div class="panelPedidos">
+                        <div class="pedidosTitlePanel">
+                            Seus pedidos
+                        </div>
+                        <div class="itensPedidosContainer">
+                            <div class="itensWrap">
+                                <!-- Card de exemplo para quando o cliente não está logado -->
+                                <div class="cardPedido">
+                                    <div class="imgProdutoPedido">
+                                        <img src="{{ url('images/teste.jpg') }}">
+                                    </div>
+                                    <div class="infoProdutoPedido">
+                                        <div class="infoPedido">
+                                            <ul class="labelPedido">
+                                                <li>Vestindo a Quebrada</li>
+                                                <li>Nike Tech Fleece</li>
+                                                <li>R$49,90</li>
+                                            </ul>
+                                            <ul class="statusPedido">
+                                                <li class="sttsPedido">Status</li>
+                                                <li class="situacaoPedido">Enviado</li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- Adicione mais cards se necessário -->
+                            </div>
+                        </div>
+                    </div>
+                @else
+                    <div class="panelPedidos">
+                        <div class="pedidosTitlePanel">
+                            Seus pedidos
+                        </div>
+                        <div class="itensPedidosContainer">
+                            <div class="containerNoRegister">
+                                <div class="noRegisterImg">
+                                    <img src="{{url('images/msgSemRegistro.png')}}" class="noContentImg">
+                                </div>
+                                <div class="noRegisterMsg">
+                                    <h4>Oops...</h4>
+                                    <p>Faça <a href="">login</a> ou <a href="">cadastre-se</a> para ver seus pedidos</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
             </div>
         </div>
     </main>
+
     <!--Import do javascript-->
-    <script src="{{('js/script.js')}}"></script>
+    <script src="{{ url('js/script.js') }}"></script>
     <!--Imports do bootstrap do body-->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
@@ -94,7 +110,6 @@
         integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
         crossorigin="anonymous"></script>
 </body>
-
 <!--Fim do body-->
 
 </html>

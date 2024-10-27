f5a0cda7e7c23aadbc87d0e4cadb93f89756d7e2
Nicoly Silva <112127566+niczonha@users.noreply.github.com>
2024-10-26 22:26:07 -0300

    Update centralAjuda.blade.php
    que saco tio

--- resources/views/centralAjuda.blade.php
+++ resources/views/centralAjuda.blade.php
@@ -1,351 +1,244 @@
-<!--Começo head-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"><!--Import do bootstrap-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" ><!--Import do fontawesome-->
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
-   
     <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/x-icon">
     <title>Perifa</title>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+        body {
+            font-family: "Poppins", sans-serif;
+        }
+
+        header {
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            background-color: #eee9e4;
+            padding: 0.5rem;
+        }
+
+        .header_direita {
+            display: flex;
+            align-items: center;
+            max-width: 1200px;
+            width: 100%;
+            justify-content: space-between;
+        }
+
+        .header-logo {
+            flex: 1;
+            display: flex;
+            align-items: center;
+        }
+
+        .logo {
+            max-width: 100%;
+        }
+
+        .ajuda {
+            color: #fff;
+            font-weight: 600;
+            font-size: 20px;
+            line-height: 10px;
+        }
+
+        .ajuda h5 {
+            margin-top: -15px;
+            line-height: 1;
+            color: #5e3e24;
+            text-align: center;
+        }
+
+        .search-bar {
+            display: flex;
+            justify-content: center;
+            margin: 20px 0;
+        }
+
+        .input-icon {
+            position: relative;
+            width: 100%;
+            max-width: 600px;
+        }
+
+        .search-input {
+            width: 100%;
+            padding: 12px 40px 12px 15px;
+            border-radius: 20px;
+            border: 1px solid #ccc;
+            font-size: 18px;
+        }
+
+        .search-input:focus {
+            outline: none;
+        }
+
+        .icon {
+            position: absolute;
+            right: 15px;
+            top: 50%;
+            transform: translateY(-50%);
+            color: #ccc;
+            font-size: 18px;
+        }
+
+        .button {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .buttons-section {
+            display: flex;
+            justify-content: center;
+            gap: 15px;
+            flex-wrap: wrap;
+            margin-bottom: 30px;
+        }
+
+        .buttons-section .button {
+            padding: 20px 20px;
+            background-color: #f4f4f4;
+            border-radius: 10px;
+            text-align: center;
+            width: 120px;
+            font-weight: bold;
+            color: #5a4020;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            transition: box-shadow 0.2s;
+        }
+
+        .buttons-section .button:hover {
+            background-color: #e0e0e0;
+            color: #3a2a1a;
+            box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3);
+            transform: scale(1.07);
+        }
+
+        .buttons-section .button:hover i {
+            color: #3a2a1a;
+        }
+
+        .buttons-section .button i {
+            font-size: 25px;
+            padding-bottom: 11px;
+        }
+
+        /* Perguntas Frequentes */
+        .faq-section {
+            display: flex;
+            justify-content: center;
+        }
+
+        .faq-box {
+            background-color: #5e3e24;
+            padding: 30px;
+            border-radius: 10px;
+            width: 100%;
+            max-width: 500px;
+            font-size: 16px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            transition: box-shadow 0.2s;
+        }
+
+        .faq-box a {
+            color: #fff;
+            text-decoration: none;
+        }
+
+        .faq-box a:hover{
+            text-decoration: underline;
+            color: rgba(255, 255, 255, 0.7);    
+        }
+
+        .main-content {
+            padding: 50px;
+        }
+
+        .main-content h1, h2 {
+            font-size: 1.5rem;
+            text-align: center;
+            font-weight: 600;
+        }
+
+      
+        @media (max-width: 768px) {
+            .search-input {
+                font-size: 16px;
+                padding: 10px 30px 10px 10px;
+            }
+
+            .buttons-section .button {
+                width: 90%; 
+                max-width: 300px; 
+            }
+
+            .main-content {
+                padding: 20px;
+            }
+
+            .main-content h1, h2 {
+                font-size: 1.25rem;
+            }
+        }
+    </style>
 </head>
-<!--Começo fim da head-->
 
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-
-   body{
-    font-family: "Poppins", sans-serif;
-   
-   }
-    header{
-        box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Adiciona a sombra */
-        background-color: #5e3e24;
-        padding: 0.5rem;
-        
-    }
-
-    .header_direita {
-    display: flex;
-    align-items: center;
-    max-width: 1200px;
-    /* Ajuste conforme necessário */
-    width: 100%;
-    justify-content: space-between;
-    /* Distribui logo e ícones */
-}
-.botao-abrir-menu i {
-    color: #fff;
-    font-size: 25px;
-}
-
-.menu-mobile {
-    background-color: #fff;
-    height: 100vh;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 999;
-    width: 0%;
-    overflow-y: auto;
-    /* Adiciona rolagem se necessário */
-    overflow: hidden;
-    transition: .2s;
-
-}
-
-.menu-mobile.abrir-menu {
-    width: 70%;
-    padding: 1rem;
-}
-
-.menu-mobile.abrir-menu~.overlay-menu {
-    display: block;
-}
-
-.menu-mobile .botao-fechar {
-    padding: 20px 5%;
-
-}
-
-.menu-mobile .botao-fechar i {
-    color: #5e3e24;
-    font-size: 25px;
-}
-
-.input-container-mobile {
-    display: flex;
-    align-items: center;
-    background-color: #f4f4f4;
-    /* Cor de fundo para o campo de entrada */
-    border-radius: 4px;
-    /* Bordas arredondadas */
-    overflow: hidden;
-    /* Remove qualquer excesso de borda */
-    width: 100%;
-    /* Garante que o contêiner ocupe toda a largura disponível */
-}
-
-.input-container-mobile .input-pesquisa {
-    flex: 1;
-    /* Faz o campo de entrada ocupar todo o espaço disponível */
-    border: none;
-    /* Remove a borda padrão */
-    padding: 10px;
-    /* Adiciona padding interno */
-    font-size: 16px;
-    /* Ajusta o tamanho da fonte */
-    border-radius: 4px;
-    /* Bordas arredondadas */
-    outline: none;
-    /* Remove o contorno padrão ao focar */
-}
-
-.input-container-mobile .botao-pesquisa {
-    background-color: #f4f4f4;
-    /* Cor de fundo para o botão */
-    border: none;
-    /* Remove a borda padrão do botão */
-    padding: 10px;
-    /* Adiciona padding interno */
-    cursor: pointer;
-    /* Muda o cursor para indicar que é clicável */
-}
-
-.header-logo {
-    flex: 1;
-    /* Faz com que a logo ocupe o espaço necessário no lado esquerdo */
-    display: flex;
-    align-items: center;
-}
-
-.logo {
-    max-width: 100%;
-    /* Ajusta a logo para se ajustar ao tamanho do contêiner */
-}
-
-/* Oculta o campo de pesquisa e o botão em telas menores */
-@media (min-width: 907px) {
-    .botao-abrir-menu {
-        display: none;
-    }
-}
-
-@media (max-width: 907px) {
-    .header_pesquisa {
-        display: none;
-        /* Oculta o campo de pesquisa e o botão de pesquisa */
-    }
-
-    .header_direita {
-        justify-content: space-between;
-        /* Garante que todos os itens fiquem alinhados corretamente */
-    }
-}
-
-
-@media (max-width: 480px) {
-    header {
-        padding: 1rem;
-    }
-
-    .header_direita {
-        flex-direction: row;
-        /* Mantém a direção em linha */
-        align-items: center;
-        /* Alinha verticalmente os itens */
-    }
-
-    .logo {
-        margin-right: 10px;
-        /* Ajusta o espaço ao redor da logo */
-    }
-   
-
-    .header_pesquisa {
-        display: none;
-        /* Garante que o campo de pesquisa e o botão de pesquisa sejam ocultos */
-    }
-
-    .header_icones {
-        display: flex;
-        align-items: center;
-    }
-
-    .header_icones button {
-        margin-left: 8px;
-    }
-
-    .header_icones i {
-        font-size: 24px;
-    }
-
-}
-
-.ajuda {
-    color: #fff;
-    font-family: "Poppins", sans-serif;
-    font-weight: 600;
-  
-    font-size: 20px;
-    line-height: 10px;
-}
-
-.ajuda h5 {
-    margin-top: -15px; 
-    line-height: 1; 
-    color: #fff; 
-    text-align: center;
-}
-
-/* Estilo da barra de pesquisa */
-.search-bar {
-  margin: 20px 0;
-  display: flex;
-  justify-content: center;
-}
-
-.search-bar input {
-  width: 100%;
-  max-width: 600px;
-  padding: 10px;
-  border-radius: 20px;
-  border: 1px solid #ccc;
-  font-size: 16px;
-}
-
-/* Botões de navegação */
-.buttons-section {
-  display: flex;
-  justify-content: center;
-  gap: 15px;
-  flex-wrap: wrap;
-  margin-bottom: 30px;
-}
-
-.buttons-section .button {
-  padding: 15px;
-  background-color: #f4f4f4;
-  border-radius: 10px;
-  text-align: center;
-  width: 120px;
-  font-weight: bold;
-  color: #5a4020;
-}
-
-/* Perguntas Frequentes */
-.faq-section {
-  display: flex;
-  justify-content: center;
-}
-
-.faq-box {
-  background-color: #a86d4b;
-  padding: 20px;
-  border-radius: 10px;
-  width: 100%;
-  max-width: 500px;
-  color: white;
-  font-size: 16px;
-}
-
-.faq-box p {
-  margin: 10px 0;
-}
-
-/* Estilos responsivos */
-@media (max-width: 768px) {
-  .buttons-section {
-    flex-direction: column;
-    gap: 10px;
-  }
-
-  .faq-box {
-    max-width: 100%;
-  }
-
-  header {
-    flex-direction: column;
-    text-align: center;
-  }
-
-  .menu-icons {
-    justify-content: center;
-    gap: 10px;
-  }
-}
-</style>
 <body>
-    
-
 <header>
-<div class="header_direita">
-            <div class="header-logo">
-                <div class="botao-abrir-menu" id="botao-menu">
-                    <i class="fa-solid fa-bars"></i>
-            </div>
-            <div class="menu-mobile" id="menu-mobile">
-                <div class="botao-fechar">
-                    <i class="fa-solid fa-x"></i>
-                </div>
-            <div class="input-container-mobile">
-                    <input type="search" placeholder="PESQUISAR PRODUTOS" class="input-pesquisa">
-                    <button class="botao-pesquisa" data-toggle="modal" data-target="#modalSair">
-                        <i class="fa-solid fa-magnifying-glass" id="pesquisa"></i>
-                    </button>
-
-            </div>
-            </div>
-            <div class="ajuda">
-            <a href="{{ url('/logo') }}">
-                            <img class="logo" src="{{url('images/logoPerifa-branca.png')}}" alt="Logo perifa">
-            </a>  
+    <div class="header_direita">
+        <div class="ajuda">
+            <a href="{{ url('/') }}">
+                <img class="logo" src="{{ url('images/logo.png') }}" alt="Logo perifa">
+            </a>
             <h5>Central de ajuda</h5>
-            </div>
-</div>
-
+        </div>
+    </div>
 </header>
 
-<main class="main-content">
-        <h1>Salve, Como podemos te ajudar ?</h1>
-        <div class="search-bar">
-            <input type="text" placeholder="Procurar...">
+<main class="container main-content">
+    <h1>Salve, Como podemos te ajudar ?</h1>
+
+    <div class="search-bar">
+        <div class="input-icon">
+            <i class="fa-solid fa-magnifying-glass icon"></i>
+            <input type="text" placeholder="Procurar..." class="search-input">
         </div>
+    </div>
 
-        <div class="buttons-section">
-            <div class="button">Pagamentos</div>
-            <div class="button">Reembolso</div>
-            <div class="button">Envios</div>
-            <div class="button">Info Gerais</div>
+    <div class="buttons-section">
+        <div class="button"><i class="fa-solid fa-credit-card"></i>Pagamentos</div>
+        <div class="button"><i class="fa-solid fa-money-bill-transfer"></i>Reembolso</div>
+        <div class="button"><i class="fa-solid fa-truck"></i>Envios</div>
+        <div class="button"><i class="fa-solid fa-circle-info"></i>Informações</div>
+    </div>
+
+    <h2>Perguntas frequentes</h2>
+    <div class="faq-section">
+        <div class="faq-box">
+            <a href=""><p>Qual a procedência dos produtos?</p></a>
+            <a href=""><p>Como são as embalagens?</p></a>
+            <a href=""><p>Posso trocar ou devolver um produto?</p></a>
+            <a href=""><p>Quais são os métodos de pagamento aceitos?</p></a>
         </div>
+    </div>
+</main>
 
-        <h2>Perguntas frequentes</h2>
-        <div class="faq-section">
-            <div class="faq-box">
-            <p>Qual a procedência dos produtos?</p>
-            <p>Como são as embalagens?</p>
-            <p>Posso trocar ou devolver um produto?</p>
-            <p>Quais são os métodos de pagamento aceitos?</p>
-            </div>
-        </div>
-        </main>
 
-  <!--Import do javascript-->
-  <script src="{{('js/script.js')}}"></script>
-    <!--Imports do bootstrap do body-->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-        crossorigin="anonymous"></script>
-                    </body>                    
-
-                    
+<script src="{{ ('js/script.js') }}"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" crossorigin="anonymous"></script>
+</body>
+</html>

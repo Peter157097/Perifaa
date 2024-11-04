75e21cffd4b05ca2176dfeefd89a3364fe7da929
Nicoly Silva <112127566+niczonha@users.noreply.github.com>
2024-11-04 15:00:00 -0300

    Update menuAdmin.blade.php

--- resources/views/includes/menuAdmin.blade.php
+++ resources/views/includes/menuAdmin.blade.php
@@ -15,7 +15,7 @@
         <li><a href="/filtro-usuarios" class="menu-item"><i class="fas fa-box-open"></i>Consultar</a></li>
         <li><a href="/perfil" class="menu-item"><i class="fas fa-user"></i>Perfil</a></li>
         <li><a href="#config" class="menu-item"><i class="fas fa-cog"></i>Configurações</a></li>
-        <li><a href="#sair" class="menu-item"><i class="fas fa-sign-out-alt"></i>Sair</a></li>
+        <li><a href="{{ url('/logout') }}" class="menu-item"><i class="fas fa-sign-out-alt"></i>Sair</a></li>
     </ul>
 </nav>
 <!-- Conteúdo principal da página -->

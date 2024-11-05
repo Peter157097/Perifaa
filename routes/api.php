7b9d3511a2ad4bceb293713485b36aef774bea07
Eduardo7070 <112709921+Eduardo7070@users.noreply.github.com>
2024-11-05 00:37:00 -0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MensagemController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/mensagens/{idVendedor}', [MensagemController::class, 'carregarHistorico']);
+Route::post('/mensagens/{idVendedor}', [MensagemController::class, 'enviarMensagem']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();

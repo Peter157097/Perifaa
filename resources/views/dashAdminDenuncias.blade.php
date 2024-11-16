1f20ff3c1add85538bad2fdd25d9a972ee1fe2a8
Nicoly Silva <112127566+niczonha@users.noreply.github.com>
2024-11-16 15:44:06 -0300

    Update dashAdminDenuncias.blade.php

--- resources/views/dashAdminDenuncias.blade.php
+++ resources/views/dashAdminDenuncias.blade.php
@@ -11,6 +11,14 @@
     <!-- Fonte customizada para o logo -->
     <link href="https://fonts.googleapis.com/css2?family=Bubblegum+Sans&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js"></script>
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <style>
      .modal-content {
